a7ee0e593dd7792f7ca78d97c593bf846f4fb1bd
Xin Liu <xhliu97@gmail.com>
2020-11-18 20:12:31 -0500

    added rotate button

--- src/main/resources/public/js/gameUtils.js
+++ src/main/resources/public/js/gameUtils.js
@@ -31,21 +31,38 @@ function drawBoard() {
    *   Submarine   (3)
    *   Patrol Boat (2)
    */
-  drawShip("#carrier", 5, 25, "#44BBA4");
-  drawShip("#battleship", 4, 125, "#ffae42");
-  drawShip("#destroyer", 3, 225, "#114B5F");
-  drawShip("#submarine", 3, 325, "#E94F37");
-  drawShip("#boat", 2, 425, "#F6F7EB");
+  drawShip("#carrier", 5, "#44BBA4", "default", null, 25);
+  drawShip("#battleship", 4, "#ffae42", "default", null, 125);
+  drawShip("#destroyer", 3, "#114B5F", "default", null, 225);
+  drawShip("#submarine", 3, "#E94F37", "default", null, 325);
+  drawShip("#boat", 2, "#F6F7EB", "default", null, 425);
+
+  localStorage.setItem("shipsDirection", "default");
 
   document.querySelector("#board").innerHTML = board;
 }
 
-function drawShip(id, length, y, fillColor) {
+/**
+ * Prepares the ships for the game
+ * @param {String} id
+ * @param {Number} length
+ * @param {String} fillColor
+ * @param {String} direction
+ * @param {Number} defaultX
+ * @param {Number} defaultY
+ */
+function drawShip(id, length, fillColor, direction, defaultX, defaultY) {
   let ship = "";
-  let x;
+  let x, y;
 
   for (let i = 0; i < length; i++) {
-    x = 75 * i + 1200;
+    if (direction === "default") {
+      x = 75 * i + 1200;
+      y = defaultY;
+    } else if (direction === "rotated") {
+      x = defaultX;
+      y = 75 * i + 25;
+    }
 
     ship += `<rect x="${x}" 
                     y="${y}"
@@ -57,4 +74,33 @@ function drawShip(id, length, y, fillColor) {
   }
 
   document.querySelector(id).innerHTML = ship;
+}
+
+/**
+ * Rotates the ships
+ */
+function rotateShips() {
+  // Get the stored ship direction value
+  const direction = localStorage.getItem("shipsDirection");
+
+  // Clear existing ships
+  document.querySelector("#carrier").innerHTML = "";
+  document.querySelector("#battleship").innerHTML = "";
+  document.querySelector("#destroyer").innerHTML = "";
+  document.querySelector("#submarine").innerHTML = "";
+  document.querySelector("#boat").innerHTML = "";
+
+  if (direction === "default") {
+    drawShip("#carrier", 5, "#44BBA4", "default", null, 25);
+    drawShip("#battleship", 4, "#ffae42", "default", null, 125);
+    drawShip("#destroyer", 3, "#114B5F", "default", null, 225);
+    drawShip("#submarine", 3, "#E94F37", "default", null, 325);
+    drawShip("#boat", 2, "#F6F7EB", "default", null, 425);
+  } else if (direction === "rotated") {
+    drawShip("#carrier", 5, "#44BBA4", "rotated", 1100, null);
+    drawShip("#battleship", 4, "#ffae42", "rotated", 1200, null);
+    drawShip("#destroyer", 3, "#114B5F", "rotated", 1300, null);
+    drawShip("#submarine", 3, "#E94F37", "rotated", 1400, null);
+    drawShip("#boat", 2, "#F6F7EB", "rotated", 1500, null);
+  }
 }
