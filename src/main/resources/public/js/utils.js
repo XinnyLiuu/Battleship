5fe1559e2086ae586ba9e0adbf9f2bac7bd5bae8
Xin Liu <xhliu97@gmail.com>
2020-11-28 20:52:23 -0500

    added logout functionality

--- src/main/resources/public/js/utils.js
+++ src/main/resources/public/js/utils.js
@@ -7,8 +7,7 @@ function getCookie(name) {
 function setCookie(name, value, days) {
   const d = new Date();
   d.setTime(d.getTime() + 24 * 60 * 60 * 1000 * days);
-  document.cookie =
-    name + "=" + value + ";path=/;expires=" + d.toGMTString();
+  document.cookie = name + "=" + value + ";path=/;expires=" + d.toGMTString();
 }
 
 function deleteCookie(name) {
